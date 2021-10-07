bd987dd88249be0aa8ec23084edffa17cdd3ef85
Alexander Nikolas Reuber <info@unconnect.net>
2021-10-07 08:24:45 +0200

    added gameloop, basic gameplay is done

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const computerPlay = function () {
  * @param {int} max the highest value to get back
  * @returns {int} a random integer from 0 to max
  */
-
 const getRandomInt = function (max) {
     return Math.floor(Math.random() * max);
 }
@@ -27,6 +26,11 @@ const getRandomInt = function (max) {
  */
 const playGameRound = function (playerSelection, computerSelection) {
 
+    let roundResult = {
+        playerPoint: 0,
+        computerPoint: 0
+    }
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
@@ -38,16 +42,32 @@ const playGameRound = function (playerSelection, computerSelection) {
     const isTie = (playerSelection === computerSelection);
 
     // Debug check the computers selection for this round.
-    console.log('Computers selection: ' + computerSelection);
     console.log('Players selection: ' + playerSelection);
+    console.log('Computers selection: ' + computerSelection);
+    
+
+    // What are we returning now? Maybe it should update an object and returns that everytime?
+    //
+    // I thing the best (right now) would be to return JUST the result of each round,
+    // for each player. 
+    // The result could be an object.
+    // The result should than be used outside this function.
 
     if (isTie) {
-        return `It's a tie, Player selected ${playerSelection} and Computer selected ${computerSelection}.`;
-    } else if (playerWonRound) {
-        return `The Player won the round, ${playerSelection} wins against ${computerSelection}!`;
+        roundResult.playerPoint = 0;
+        roundResult.computerPoint = 0;
     }
 
-    return `The Computer won the round, ${computerSelection} wins against ${playerSelection}!`;
+    if (playerWonRound) {
+        roundResult.playerPoint = 1;
+        roundResult.computerPoint = 0;
+    } else if (!playerWonRound && !isTie) {
+        roundResult.playerPoint = 0;
+        roundResult.computerPoint = 1;
+    }
+
+    console.log(roundResult);
+    return roundResult;
 
 }
 
@@ -59,6 +79,67 @@ const playerPlay = function () {
     return prompt('Please make your choice (Rock, Paper or Scissor): ', computerPlay());
 }
 
-// Debug play randomly selection for both player for testing the game logic.
-console.log(playGameRound(playerPlay(), computerPlay()));
+/**
+ * The main game loop
+ * 
+ * @returns
+ */
+
+const game = function () {
+    const maxGameRounds = 5;
+    let gameIsNotFinished = true;
+    let gameRound = 1; // Sentinel
+
+    let playerSelection;
+    let computerSelection;
+
+    let gameResult = {
+        playerPoints: 0,
+        computerPoints: 0
+    };
+
+    let winner = '';
+
+    // Loop while game is not finished. Inside the loop we should update the gameround counter.
+    while (gameIsNotFinished) {
+
+        let roundResult = {
+            playerPoint: 0,
+            computerPoint: 0
+        };
+
+        console.log(gameRound, gameIsNotFinished);
+
+        playerSelection = playerPlay();
+        computerSelection = computerPlay();
+        roundResult = playGameRound(playerSelection, computerSelection);
+        // The returned result should be used here.
+
+        gameResult.playerPoints += roundResult.playerPoint;
+        gameResult.computerPoints += roundResult.computerPoint;
+
+        console.log(`Player vs. Computer ${gameResult.playerPoints} : ${gameResult.computerPoints}`)
+
+        // Update Sentinel
+        gameRound++;
+        gameIsNotFinished = (gameRound > maxGameRounds) ? false : true;
+    }
+
+    // Check who is the winner and print it out.
+    if(gameResult.playerPoints > gameResult.computerPoints) {
+        winner = 'the Player';
+    } else if (gameResult.playerPoints < gameResult.computerPoints) {
+        winner = 'the Computer';
+    } else {
+        winner = 'Nobody';
+    }
+
+    console.log(`The winner of the game is ${winner}!`);
+
+}
+
+game();
+
+// // Debug play randomly selection for both player for testing the game logic.
+// console.log(playGameRound(playerSelection, computerSelection));
 
